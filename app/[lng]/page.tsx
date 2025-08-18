20ac4d8a42cdbc6f9480208fbd12f72e1c731bfe
gyosic <gyosic@gractor,com>
2025-08-18 16:44:29 +0900

    fix: lng -> lngParams

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -11,17 +11,17 @@ interface DefaultPageProps extends React.PropsWithChildren {
 }
 
 export default async function Page({ params }: DefaultPageProps) {
-  const { lng } = await params;
+  const { lng: lngParams } = await params;
   return (
     <div className="flex w-full justify-between">
       {/* LEFT SIDE  */}
       <FramerWrapper className="flex h-full w-auto flex-col justify-start gap-4" y={0} x={-100}>
-        <HeroTexts lng={lng} personal={personal} />
+        <HeroTexts lng={lngParams} personal={personal} />
         {/* <div className="h-fit w-full p-4 flex gap-4">
           <SocialLinks />
         </div>
         <DownLoadResumeBtn /> */}
-        <FortuneBtn lng={lng} personal={personal} />
+        <FortuneBtn lng={lngParams} personal={personal} />
       </FramerWrapper>
       {/* RIGHT SIDE image  */}
       <FramerWrapper className="relative block h-full w-[47%] max-lg:hidden" y={0} x={100}>
