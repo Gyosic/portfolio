82b3cca74a9d9268793805846dfb1877ede81555
gyosic <gyosic@gractor,com>
2025-08-20 17:02:48 +0900

    fix: fetch 예외처리

--- app/[lng]/education/page.tsx
+++ app/[lng]/education/page.tsx
@@ -2,28 +2,42 @@ import { api } from "@/config";
 import { Education } from "./Education";
 
 const getEducations = async () => {
-  const res = await fetch(new URL("/api/educations", api.baseurl), {
-    method: "POST",
-    body: JSON.stringify({ sort: [{ id: "start", desc: true }] }),
-  });
+  try {
+    const res = await fetch(new URL("/api/educations", api.baseurl), {
+      method: "POST",
+      body: JSON.stringify({ sort: [{ id: "start", desc: true }] }),
+    });
 
-  if (!res.ok) return { rows: [] };
+    if (!res.ok) return [];
 
-  const { rows = [] } = await res.json();
+    const { rows = [] } = await res.json();
 
-  return rows.length
-    ? rows
-    : [
-        {
-          degree: "Bachelor of Computer Application",
-          institution: "Ranchi University Jharkhand",
-          location: "Doranda College Ranchi",
-          start: "2023-05-02",
-          end: "",
-          description:
-            "I am currently Studying Bachelor of Computer Application form Doranda College Ranchi a Goverment College of Ranchi. The program has provided me with a well-rounded education, covering both theoretical foundations and practical applications of computer science.",
-        },
-      ];
+    return !!rows?.length
+      ? rows
+      : [
+          {
+            degree: "Bachelor of Computer Application",
+            institution: "Ranchi University Jharkhand",
+            location: "Doranda College Ranchi",
+            start: "2023-05-02",
+            end: "",
+            description:
+              "I am currently Studying Bachelor of Computer Application form Doranda College Ranchi a Goverment College of Ranchi. The program has provided me with a well-rounded education, covering both theoretical foundations and practical applications of computer science.",
+          },
+        ];
+  } catch {
+    return [
+      {
+        degree: "Bachelor of Computer Application",
+        institution: "Ranchi University Jharkhand",
+        location: "Doranda College Ranchi",
+        start: "2023-05-02",
+        end: "",
+        description:
+          "I am currently Studying Bachelor of Computer Application form Doranda College Ranchi a Goverment College of Ranchi. The program has provided me with a well-rounded education, covering both theoretical foundations and practical applications of computer science.",
+      },
+    ];
+  }
 };
 export default async function EducationPage() {
   const educations = await getEducations();
